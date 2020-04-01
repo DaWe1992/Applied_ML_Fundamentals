46e97f9176ca7601aef0c72e46f0d4b0a27f6810
DaWe1992 <daniel.wehner92@gmx.de>
2020-04-01 15:06:07 +0200

    refactoring

--- 06_python/main.py
+++ 06_python/main.py
@@ -234,12 +234,12 @@ def unsupervised_learning():
 #    c_assign = dbscan.fit(X)
     
     # OPTICS clustering
-#    optics = OPTICS(eps=3.00, eps_=1.00, min_pts=4, plot_reach=True)
-#    c_assign = optics.fit(X)
+    optics = OPTICS(eps=100.00, eps_=1.00, min_pts=4, plot_reach=True)
+    c_assign = optics.fit(X)
     
     # affinity propagation clustering
-    ap = AffinityPropagation()
-    c_assign = ap.fit(X, damping=0.9, n_iter=50, plot=True)
+#    ap = AffinityPropagation()
+#    c_assign = ap.fit(X, damping=0.9, n_iter=40, plot=True)
     
     # mean-shift clustering
 #    ms = MeanShift()
