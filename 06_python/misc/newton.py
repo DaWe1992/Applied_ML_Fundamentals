f8de87ff38bf0d847bd7809ac9570a3fe8266b20
DaWe1992 <daniel.wehner92@gmx.de>
2020-04-09 15:37:49 +0200

    newton rosenbrock

--- 06_python/misc/newton.py
+++ 06_python/misc/newton.py
@@ -21,7 +21,7 @@ from mpl_toolkits import mplot3d
 
 def optimize():
     """
-    Optimizes function x^2 + y^2
+    Optimizes function x^2 + y^2.
     """
     # function x^2 + y^2
     # (optimum at [0, 0])
@@ -41,6 +41,33 @@ def optimize():
     print("Optimum: {0}".format(x))
     
     
+def optimize_rosenbrock():
+    """
+    Optimizes the rosenbrock function.
+    """
+    # initial guess
+    x = np.asarray([5, 8])
+    
+    # compute Hessian and gradient
+    for _ in range(5):
+        H = np.asarray([
+            [-400 * x[1] + 1200 * x[0]**2 + 2, -400 * x[0]],
+            [-400 * x[0], 200]
+        ])
+        g = np.asarray([
+            -400 * x[0] * (x[1] - x[0]**2) + 2 * (x[0] - 1),
+            200 * (x[1] - x[0]**2)
+        ])
+    
+        # compute update
+        delta_x = -np.linalg.inv(H) @ g
+    
+        # update x
+        x = x + delta_x
+        
+    print("Optimum: {0}".format(x))
+    
+    
 def plot():
     """
     Plots function.
@@ -48,15 +75,18 @@ def plot():
     def f(x, y):
         return x**2 + y**2
     
-    x = np.linspace(-6, 6, 30)
-    y = np.linspace(-6, 6, 30)
+    def rosenbrock(x, y):
+        return 100 * (y - x**2)**2 + (x - 1)**2
+    
+    x = np.linspace(-6, 6, 300)
+    y = np.linspace(-6, 6, 300)
     
     X, Y = np.meshgrid(x, y)
-    Z = f(X, Y)
+    Z = rosenbrock(X, Y)
     
-    plt.figure()
+    plt.figure(figsize=(12.00, 7.00))
     ax = plt.axes(projection="3d")
-    ax.contour3D(X, Y, Z, 50, cmap="binary")
+    ax.contour3D(X, Y, Z, 50, cmap="viridis")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_zlabel("z");
@@ -69,6 +99,6 @@ def plot():
 # -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    optimize()
+    optimize_rosenbrock()
     plot()
     
