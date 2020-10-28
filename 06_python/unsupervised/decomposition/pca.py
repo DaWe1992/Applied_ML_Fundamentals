6e989c18aa25cd3473df1e399f3da2706b529359
DaWe1992 <daniel.wehner92@gmx.de>
2020-10-28 11:11:05 +0100

    pca

--- 06_python/unsupervised/decomposition/pca.py
+++ 06_python/unsupervised/decomposition/pca.py
@@ -53,3 +53,45 @@ class PCA():
         # transform the samples onto the new subspace
         return X @ U
     
+    
+## -----------------------------------------------------------------------------
+## Plot eigenvectors
+## -----------------------------------------------------------------------------
+#
+#class Arrow3D(FancyArrowPatch):
+#    def __init__(self, xs, ys, zs, *args, **kwargs):
+#        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+#        self._verts3d = xs, ys, zs
+#
+#    def draw(self, renderer):
+#        xs3d, ys3d, zs3d = self._verts3d
+#        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+#        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+#        FancyArrowPatch.draw(self, renderer)
+#
+#fig = plt.figure(figsize=(7, 7))
+#ax = fig.add_subplot(111, projection="3d")
+#
+#ax.plot(
+#    data_set[0,:], data_set[1,:], data_set[2,:], "o",
+#    markersize=8, color="green", alpha=0.2
+#)
+#ax.plot(
+#    [mean[0][0]], [mean[1][0]], [mean[2][0]], "o",
+#    markersize=10, color="red", alpha=0.5
+#)
+#
+#for v in eig_vec_cov.T:
+#    a = Arrow3D(
+#        [mean[0][0], v[0]], [mean[1][0], v[1]], [mean[2][0], v[2]],
+#        mutation_scale=20, lw=3, arrowstyle="-|>", color="r"
+#    )
+#    ax.add_artist(a)
+#    
+#ax.set_xlabel("x_values")
+#ax.set_ylabel("y_values")
+#ax.set_zlabel("z_values")
+#
+#plt.title("Eigenvectors")
+#
+#plt.show()
